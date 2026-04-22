4d0b03dba41daf196fd0489f799214f320ae2780
npmvlKP <npmvlkp@github.com>
2026-04-22 20:14:31 +0530

    refactor(execution): remove deprecated ZerodhaTokenManager

--- src/iatb/execution/__init__.py
+++ src/iatb/execution/__init__.py
@@ -2,9 +2,6 @@
 Execution layer contracts and adapters.
 """
 
-import warnings
-
-from iatb.broker.token_manager import ZerodhaTokenManager
 from iatb.execution.base import ExecutionResult, Executor, OrderRequest
 from iatb.execution.ccxt_executor import CCXTExecutor
 from iatb.execution.live_gate import (
@@ -18,15 +15,6 @@ from iatb.execution.order_manager import OrderManager
 from iatb.execution.paper_executor import PaperExecutor
 from iatb.execution.transaction_costs import estimate_round_trip_cost, estimate_single_side_cost
 from iatb.execution.zerodha_connection import ZerodhaConnection, ZerodhaSession
-
-# Emit deprecation warning when importing from execution module
-warnings.warn(
-    "Direct import of ZerodhaTokenManager from iatb.execution is deprecated. "
-    "Import from iatb.broker.token_manager instead. "
-    "This module will be removed in a future release.",
-    DeprecationWarning,
-    stacklevel=2,
-)
 
 __all__ = [
     "CCXTExecutor",
@@ -44,5 +32,4 @@ __all__ = [
     "require_live_trading_enabled",
     "ZerodhaConnection",
     "ZerodhaSession",
-    "ZerodhaTokenManager",
 ]
